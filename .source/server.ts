445a0805a858d6efcc299526d1a3b50cb711f57a
Mohamad Irfianto <81077632+emarai@users.noreply.github.com>
2026-02-27 15:18:59 +0700

    docs: rewrite and reorder

--- .source/server.ts
+++ .source/server.ts
@@ -1,19 +1,18 @@
 // @ts-nocheck
-import * as __fd_glob_20 from "../content/docs/concepts/on-chain-oracle.mdx?collection=docs"
-import * as __fd_glob_19 from "../content/docs/concepts/index.mdx?collection=docs"
-import * as __fd_glob_18 from "../content/docs/concepts/events.mdx?collection=docs"
-import * as __fd_glob_17 from "../content/docs/concepts/entities.mdx?collection=docs"
-import * as __fd_glob_16 from "../content/docs/concepts/deployment.mdx?collection=docs"
-import * as __fd_glob_15 from "../content/docs/concepts/architecture.mdx?collection=docs"
-import * as __fd_glob_14 from "../content/docs/concepts/ai-engine.mdx?collection=docs"
-import * as __fd_glob_13 from "../content/docs/api-reference/worlds.mdx?collection=docs"
-import * as __fd_glob_12 from "../content/docs/api-reference/index.mdx?collection=docs"
-import * as __fd_glob_11 from "../content/docs/api-reference/events.mdx?collection=docs"
-import * as __fd_glob_10 from "../content/docs/api-reference/entity-schemas.mdx?collection=docs"
-import * as __fd_glob_9 from "../content/docs/api-reference/entity-instances.mdx?collection=docs"
-import * as __fd_glob_8 from "../content/docs/api-reference/deployments.mdx?collection=docs"
-import * as __fd_glob_7 from "../content/docs/api-reference/authentication.mdx?collection=docs"
-import * as __fd_glob_6 from "../content/docs/api-reference/ai-models.mdx?collection=docs"
+import * as __fd_glob_19 from "../content/docs/api-reference/worlds.mdx?collection=docs"
+import * as __fd_glob_18 from "../content/docs/api-reference/index.mdx?collection=docs"
+import * as __fd_glob_17 from "../content/docs/api-reference/events.mdx?collection=docs"
+import * as __fd_glob_16 from "../content/docs/api-reference/entity.mdx?collection=docs"
+import * as __fd_glob_15 from "../content/docs/api-reference/entity-instances.mdx?collection=docs"
+import * as __fd_glob_14 from "../content/docs/api-reference/deployments.mdx?collection=docs"
+import * as __fd_glob_13 from "../content/docs/api-reference/ai-models.mdx?collection=docs"
+import * as __fd_glob_12 from "../content/docs/concepts/on-chain-oracle.mdx?collection=docs"
+import * as __fd_glob_11 from "../content/docs/concepts/index.mdx?collection=docs"
+import * as __fd_glob_10 from "../content/docs/concepts/events.mdx?collection=docs"
+import * as __fd_glob_9 from "../content/docs/concepts/entities.mdx?collection=docs"
+import * as __fd_glob_8 from "../content/docs/concepts/deployment.mdx?collection=docs"
+import * as __fd_glob_7 from "../content/docs/concepts/architecture.mdx?collection=docs"
+import * as __fd_glob_6 from "../content/docs/concepts/ai-engine.mdx?collection=docs"
 import * as __fd_glob_5 from "../content/docs/quickstart.mdx?collection=docs"
 import * as __fd_glob_4 from "../content/docs/index.mdx?collection=docs"
 import * as __fd_glob_3 from "../content/docs/environment.mdx?collection=docs"
@@ -28,4 +27,4 @@ const create = server<typeof Config, import("fumadocs-mdx/runtime/types").Intern
   }
 }>({"doc":{"passthroughs":["extractedReferences"]}});
 
-export const docs = await create.docs("docs", "content/docs", {"meta.json": __fd_glob_0, "api-reference/meta.json": __fd_glob_1, "concepts/meta.json": __fd_glob_2, }, {"environment.mdx": __fd_glob_3, "index.mdx": __fd_glob_4, "quickstart.mdx": __fd_glob_5, "api-reference/ai-models.mdx": __fd_glob_6, "api-reference/authentication.mdx": __fd_glob_7, "api-reference/deployments.mdx": __fd_glob_8, "api-reference/entity-instances.mdx": __fd_glob_9, "api-reference/entity-schemas.mdx": __fd_glob_10, "api-reference/events.mdx": __fd_glob_11, "api-reference/index.mdx": __fd_glob_12, "api-reference/worlds.mdx": __fd_glob_13, "concepts/ai-engine.mdx": __fd_glob_14, "concepts/architecture.mdx": __fd_glob_15, "concepts/deployment.mdx": __fd_glob_16, "concepts/entities.mdx": __fd_glob_17, "concepts/events.mdx": __fd_glob_18, "concepts/index.mdx": __fd_glob_19, "concepts/on-chain-oracle.mdx": __fd_glob_20, });
+export const docs = await create.docs("docs", "content/docs", {"meta.json": __fd_glob_0, "api-reference/meta.json": __fd_glob_1, "concepts/meta.json": __fd_glob_2, }, {"environment.mdx": __fd_glob_3, "index.mdx": __fd_glob_4, "quickstart.mdx": __fd_glob_5, "concepts/ai-engine.mdx": __fd_glob_6, "concepts/architecture.mdx": __fd_glob_7, "concepts/deployment.mdx": __fd_glob_8, "concepts/entities.mdx": __fd_glob_9, "concepts/events.mdx": __fd_glob_10, "concepts/index.mdx": __fd_glob_11, "concepts/on-chain-oracle.mdx": __fd_glob_12, "api-reference/ai-models.mdx": __fd_glob_13, "api-reference/deployments.mdx": __fd_glob_14, "api-reference/entity-instances.mdx": __fd_glob_15, "api-reference/entity.mdx": __fd_glob_16, "api-reference/events.mdx": __fd_glob_17, "api-reference/index.mdx": __fd_glob_18, "api-reference/worlds.mdx": __fd_glob_19, });

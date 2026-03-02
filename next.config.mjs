ed2cf73a7f83759ae0758f71f576916c198f0095
Mohamad Irfianto <81077632+emarai@users.noreply.github.com>
2026-03-02 20:00:46 +0700

    fix: llms auto

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,25 @@
-import { createMDX } from 'fumadocs-mdx/next';
+import { createMDX } from "fumadocs-mdx/next";
 
 const nextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
+        protocol: "https",
+        hostname: "images.unsplash.com",
       },
       {
-        protocol: 'https',
-        hostname: 'source.unsplash.com',
+        protocol: "https",
+        hostname: "source.unsplash.com",
       },
     ],
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/docs/:path*.mdx",
+        destination: "/llms.mdx/docs/:path*",
+      },
+    ];
   },
 };
 
